a8e779fb9576f9341c5ab900dc47b6363ac36599
Mitsevich Maxim <max_mitsevich@mail.ru>
2022-11-14 02:57:30 +0300

    pu pu pu

--- src/details/unit_tests/src/producers_repository_test.cpp
+++ src/details/unit_tests/src/producers_repository_test.cpp
@@ -1,62 +1,67 @@
-#include "producers_repository_test.h"
-#include "database_exceptions.h"
-#include "yamlcpp_config.h"
-
-ProducersRepositoryTest::ProducersRepositoryTest() {
-  std::shared_ptr<BaseConfig> config(new YamlCppConfig(ConfFileName));
-  test_rep =
-      std::shared_ptr<StockDbTest>(new StockDbTest(config, ConfStockDbSection));
-
-  producers_rep = std::shared_ptr<PostgresProducersRepository>(
-      new PostgresProducersRepository(config, ConfProducersDbSection));
-}
-
-void ProducersRepositoryTest::SetUp() {
-  test_rep->clean_database();
-}
-
-void ProducersRepositoryTest::TearDown() {
-  test_rep->clean_database();
-}
-
-TEST_F(ProducersRepositoryTest, create_producer) {
-  DetailsProducerData producer = producer_mother_object_.create_producer_data();
-
-  size_t producer_id = producers_rep->create(producer);
-
-  auto producer_res = test_rep->read_producer(producer_id);
-  ASSERT_TRUE(producer_res.country() == producer.country());
-  ASSERT_TRUE(producer_res.name() == producer.name());
-}
-
-TEST_F(ProducersRepositoryTest, read_producer) {
-  DetailsProducerData producer = producer_mother_object_.create_producer_data();
-  size_t producer_id = test_rep->create_producer(producer);
-
-  auto producer_res = producers_rep->read(producer_id);
-
-  ASSERT_TRUE(producer_res.country() == producer.country());
-  ASSERT_TRUE(producer_res.name() == producer.name());
-}
-
-TEST_F(ProducersRepositoryTest, update_producer) {
-  DetailsProducerData producer = producer_mother_object_.create_producer_data();
-  size_t producer_id = test_rep->create_producer(producer);
-  DetailsProducer producer_updated(
-      producer_id, producer.country() + producer.name(), producer.country());
-
-  producers_rep->update(producer_updated);
-
-  auto producer_res = test_rep->read_producer(producer_id);
-  ASSERT_TRUE(producer_res.country() == producer_updated.country());
-  ASSERT_TRUE(producer_res.name() == producer_updated.name());
-}
-
-TEST_F(ProducersRepositoryTest, delete_producer) {
-  DetailsProducerData producer = producer_mother_object_.create_producer_data();
-  size_t producer_id = test_rep->create_producer(producer);
-
-  producers_rep->delete_(producer_id);
-
-  EXPECT_THROW(test_rep->read_producer(producer_id), DatabaseNotFoundException);
-}
+// #include "producers_repository_test.h"
+// #include "database_exceptions.h"
+// #include "yamlcpp_config.h"
+//
+// ProducersRepositoryTest::ProducersRepositoryTest() {
+//   std::shared_ptr<BaseConfig> config(new YamlCppConfig(ConfFileName));
+//   test_rep =
+//       std::shared_ptr<StockDbTest>(new StockDbTest(config,
+//       ConfStockDbSection));
+//
+//   producers_rep = std::shared_ptr<PostgresProducersRepository>(
+//       new PostgresProducersRepository(config, ConfProducersDbSection));
+// }
+//
+// void ProducersRepositoryTest::SetUp() {
+//   test_rep->clean_database();
+// }
+//
+// void ProducersRepositoryTest::TearDown() {
+//   test_rep->clean_database();
+// }
+//
+// TEST_F(ProducersRepositoryTest, create_producer) {
+//   DetailsProducerData producer =
+//   producer_mother_object_.create_producer_data();
+//
+//   size_t producer_id = producers_rep->create(producer);
+//
+//   auto producer_res = test_rep->read_producer(producer_id);
+//   ASSERT_TRUE(producer_res.country() == producer.country());
+//   ASSERT_TRUE(producer_res.name() == producer.name());
+// }
+//
+// TEST_F(ProducersRepositoryTest, read_producer) {
+//   DetailsProducerData producer =
+//   producer_mother_object_.create_producer_data(); size_t producer_id =
+//   test_rep->create_producer(producer);
+//
+//   auto producer_res = producers_rep->read(producer_id);
+//
+//   ASSERT_TRUE(producer_res.country() == producer.country());
+//   ASSERT_TRUE(producer_res.name() == producer.name());
+// }
+//
+// TEST_F(ProducersRepositoryTest, update_producer) {
+//   DetailsProducerData producer =
+//   producer_mother_object_.create_producer_data(); size_t producer_id =
+//   test_rep->create_producer(producer); DetailsProducer producer_updated(
+//       producer_id, producer.country() + producer.name(), producer.country());
+//
+//   producers_rep->update(producer_updated);
+//
+//   auto producer_res = test_rep->read_producer(producer_id);
+//   ASSERT_TRUE(producer_res.country() == producer_updated.country());
+//   ASSERT_TRUE(producer_res.name() == producer_updated.name());
+// }
+//
+// TEST_F(ProducersRepositoryTest, delete_producer) {
+//   DetailsProducerData producer =
+//   producer_mother_object_.create_producer_data(); size_t producer_id =
+//   test_rep->create_producer(producer);
+//
+//   producers_rep->delete_(producer_id);
+//
+//   EXPECT_THROW(test_rep->read_producer(producer_id),
+//   DatabaseNotFoundException);
+// }
--- src/details/unit_tests/src/swaps_repository_test.cpp
+++ src/details/unit_tests/src/swaps_repository_test.cpp
@@ -1,62 +1,63 @@
-#include "swaps_repository_test.h"
-#include "database_exceptions.h"
-#include "yamlcpp_config.h"
-
-SwapsRepositoryTest::SwapsRepositoryTest() {
-  std::shared_ptr<BaseConfig> config(new YamlCppConfig(ConfFileName));
-  test_rep =
-      std::shared_ptr<StockDbTest>(new StockDbTest(config, ConfStockDbSection));
-
-  swaps_rep = std::shared_ptr<PostgresSwapsRepository>(
-      new PostgresSwapsRepository(config, ConfSwapsDbSection));
-}
-
-void SwapsRepositoryTest::SetUp() {
-  test_rep->clean_database();
-
-  details_ = detail_mother_object.create_details();
-
-  for (auto& detail : details_) {
-    DetailsProducerData producer =
-        producer_mother_object.create_producer_data();
-
-    size_t producer_id = test_rep->create_producer(producer);
-    detail.set_producer_id(producer_id);
-    test_rep->create(detail);
-  }
-}
-
-void SwapsRepositoryTest::TearDown() {
-  test_rep->clean_database();
-}
-
-TEST_F(SwapsRepositoryTest, create_swap) {
-  Detail detail_src = details_[0];
-  Detail detail_dst = details_[details_.size() - 1];
-
-  swaps_rep->create(detail_src.part_number(), detail_dst.part_number());
-
-  auto swaps_res = test_rep->read_swap(detail_src.part_number());
-  ASSERT_EQ(detail_dst.part_number(), swaps_res[0]);
-}
-
-TEST_F(SwapsRepositoryTest, read_swap) {
-  Detail detail_src = details_[0];
-  Detail detail_dst = details_[details_.size() - 1];
-  test_rep->create_swap(detail_src.part_number(), detail_dst.part_number());
-
-  auto swaps_res = swaps_rep->read(detail_src.part_number());
-
-  ASSERT_EQ(detail_dst.part_number(), swaps_res[0]);
-}
-
-TEST_F(SwapsRepositoryTest, delete_swap) {
-  Detail detail_src = details_[0];
-  Detail detail_dst = details_[details_.size() - 1];
-  test_rep->create_swap(detail_src.part_number(), detail_dst.part_number());
-
-  swaps_rep->delete_(detail_src.part_number(), detail_dst.part_number());
-
-  auto swaps_res = test_rep->read_swap(detail_src.part_number());
-  ASSERT_TRUE(swaps_res.empty());
-}
+// #include "swaps_repository_test.h"
+// #include "database_exceptions.h"
+// #include "yamlcpp_config.h"
+//
+// SwapsRepositoryTest::SwapsRepositoryTest() {
+//   std::shared_ptr<BaseConfig> config(new YamlCppConfig(ConfFileName));
+//   test_rep =
+//       std::shared_ptr<StockDbTest>(new StockDbTest(config,
+//       ConfStockDbSection));
+//
+//   swaps_rep = std::shared_ptr<PostgresSwapsRepository>(
+//       new PostgresSwapsRepository(config, ConfSwapsDbSection));
+// }
+//
+// void SwapsRepositoryTest::SetUp() {
+//   test_rep->clean_database();
+//
+//   details_ = detail_mother_object.create_details();
+//
+//   for (auto& detail : details_) {
+//     DetailsProducerData producer =
+//         producer_mother_object.create_producer_data();
+//
+//     size_t producer_id = test_rep->create_producer(producer);
+//     detail.set_producer_id(producer_id);
+//     test_rep->create(detail);
+//   }
+// }
+//
+// void SwapsRepositoryTest::TearDown() {
+//   test_rep->clean_database();
+// }
+//
+// TEST_F(SwapsRepositoryTest, create_swap) {
+//   Detail detail_src = details_[0];
+//   Detail detail_dst = details_[details_.size() - 1];
+//
+//   swaps_rep->create(detail_src.part_number(), detail_dst.part_number());
+//
+//   auto swaps_res = test_rep->read_swap(detail_src.part_number());
+//   ASSERT_EQ(detail_dst.part_number(), swaps_res[0]);
+// }
+//
+// TEST_F(SwapsRepositoryTest, read_swap) {
+//   Detail detail_src = details_[0];
+//   Detail detail_dst = details_[details_.size() - 1];
+//   test_rep->create_swap(detail_src.part_number(), detail_dst.part_number());
+//
+//   auto swaps_res = swaps_rep->read(detail_src.part_number());
+//
+//   ASSERT_EQ(detail_dst.part_number(), swaps_res[0]);
+// }
+//
+// TEST_F(SwapsRepositoryTest, delete_swap) {
+//   Detail detail_src = details_[0];
+//   Detail detail_dst = details_[details_.size() - 1];
+//   test_rep->create_swap(detail_src.part_number(), detail_dst.part_number());
+//
+//   swaps_rep->delete_(detail_src.part_number(), detail_dst.part_number());
+//
+//   auto swaps_res = test_rep->read_swap(detail_src.part_number());
+//   ASSERT_TRUE(swaps_res.empty());
+// }
