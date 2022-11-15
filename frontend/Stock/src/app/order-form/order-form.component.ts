965394605c1a11aeb08ae0fe0cdb9acdfb728177
Mitsevich Maxim <max_mitsevich@mail.ru>
2022-11-15 09:04:53 +0300

    pu pu pu

--- frontend/Stock/src/app/order-form/order-form.component.ts
+++ frontend/Stock/src/app/order-form/order-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StockDetailService } from '../stock-detail.service';
 import { DetailInStock } from '../stock-detail';
 import { Label } from '../left-bar/left-bar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-form',
@@ -12,17 +13,20 @@ export class OrderFormComponent implements OnInit {
   order: DetailInStock = {part_number: '', quantity: 0}
   label: Label = Label.STOCK;
 
-  constructor(private stockService: StockDetailService) { }
+  constructor(private stockService: StockDetailService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
   putDetail() {
     this.stockService.putDetailInStock(this.order).subscribe();
+    this.router.navigateByUrl('/stock');
   }
 
   takeDetail() {
     this.stockService.takeDetailFromStock(this.order).subscribe();
+    this.router.navigateByUrl('/stock');
   }
 
 }
